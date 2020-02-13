e7be0f0345f56bdb85f067f5bf5761a3f87e1197
bypie5 <bypie966@gmail.com>
2020-02-13 01:03:13 -0800

    Ignoring particularly long test to speed up coverage calculation

--- src/test/java/org/jsoup/parser/ParserIT.java
+++ src/test/java/org/jsoup/parser/ParserIT.java
@@ -1,12 +1,15 @@
 package org.jsoup.parser;
 
 import org.junit.Test;
+import org.junit.Ignore;
 
 /**
  * Longer running Parser tests.
  */
 
 public class ParserIT {
+
+    @Ignore
     @Test
     public void testIssue1251() {
         // https://github.com/jhy/jsoup/issues/1251
